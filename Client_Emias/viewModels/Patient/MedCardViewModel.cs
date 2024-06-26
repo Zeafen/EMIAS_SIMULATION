c27f4ff25c51423ff099c4f1d7f3a44a40fcc232
Zeafen <savin3115@gmail.com>
2024-06-26 09:09:05 +0300

    Replacing Patient namespace

--- Client_Emias/viewModels/Patient/MedCardViewModel.cs
+++ Client_Emias/viewModels/Patient/MedCardViewModel.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Client_Emias.viewModels.Patient
+namespace Client_Emias.viewModels.PatientVMs
 {
     public class MedcardViewModel : INotifyPropertyChanged
     {
